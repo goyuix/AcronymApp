c3d2654fd1ca3a3fbcec3df14258c52d50b0f8c9
Goyuix <goyuix@gmail.com>
2016-04-26 14:09:19 -0600

    moved render code to separate function

--- AppRead/Home/Home.js
+++ AppRead/Home/Home.js
@@ -11,6 +11,24 @@
             displayItemDetails();
         });
     };
+    
+    // render function to display the acronyms
+    function displayAcronyms(matches) {
+        // check for acronyms to be loaded, if not return back to this function in 500ms
+        if (Object.keys(app.acronyms).length < 1) {
+            setTimeout(function(){displayAcronyms(matches)},500);
+        }
+        
+        if (matches && matches.length) {
+                for (var i=0;i<matches.length;i++) {
+                    html.push('<li><b>'+matches[i]+'</b><br/>'+(app.acronyms[matches[i]] ? app.acronyms[matches[i]] : 'No matching definition'))+'</li>';
+                }
+                $("#body").html('<ul>'+html.join(',')+'</ul>');
+            } else {
+                $("#body").text('No acronyms found in this message');
+                app.showNotification('Notice - No acronyms found','Sorry, no acronyms were found in the body of this message');
+            }
+    }
 
     // Displays the "Subject" and "From" fields, based on the current mail item
     function displayItemDetails() {
@@ -34,16 +52,8 @@
         Office.context.mailbox.item.body.getAsync("text", {}, function (result) {
             var html = [];
             var matches = $.unique(result.value.match(/\b[A-Z]{3,}\b/g));
-            if (matches && matches.length) {
-                for (var i=0;i<matches.length;i++) {
-                    html.push('<li><b>'+matches[i]+'</b><br/>'+(app.acronyms[matches[i]] ? app.acronyms[matches[i]] : 'No matching definition'))+'</li>';
-                }
-                $("#body").html('<ul>'+html.join(',')+'</ul>');
-            } else {
-                $("#body").text('No acronyms found in this message');
-                app.showNotification('Notice - No acronyms found','Sorry, no acronyms were found in the body of this message');
-            }
-        })
+            displayAcronyms(matches);
+        });
         
     }
 })();
